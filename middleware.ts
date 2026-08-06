94137744b7b21eee027291a198929eea58ba70cb
Shardul <kumarshardul1998@gmail.com>
2026-08-07 02:28:58 +0530

    fix: csp

--- middleware.ts
+++ middleware.ts
@@ -39,10 +39,10 @@ export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const isDev = process.env.NODE_ENV !== "production";
 
-  // Strict CSP removing 'unsafe-inline' & 'unsafe-eval' from script-src in production
+  // Content Security Policy
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ""};
+    script-src 'self' 'unsafe-inline' 'unsafe-eval';
     style-src 'self' 'unsafe-inline';
     img-src 'self' data: blob: https:;
     font-src 'self' data:;
@@ -50,7 +50,7 @@ export function middleware(request: NextRequest) {
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
-    connect-src 'self' https: wss:;
+    connect-src 'self' https: wss: http: ws:;
     upgrade-insecure-requests;
   `
     .replace(/\s{2,}/g, " ")
