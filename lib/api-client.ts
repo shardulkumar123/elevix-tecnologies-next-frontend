702f0b1522d6dd7b4b4cb9ef769f2bb733782fa5
Shardul <kumarshardul1998@gmail.com>
2026-08-07 01:03:51 +0530

    feat: added middleware

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,7 +1,7 @@
 import { ERROR_MESSAGES, LOCAL_STORAGE_KEYS } from "@/constants";
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
